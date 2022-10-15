c8b466a0bda27c58b1cddccca0944ce8aa1432ef
Ahmed Adnan <bsse1131@iit.du.ac.bd>
2022-10-16 04:52:24 +0600

    body routing fixed

---
+++ src/components/body/Body.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Menu from './Menu';
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+import { Route, Routes, Redirect } from 'react-router-dom';
+
+const Body = () => {
+    return (
+        <div>
+            <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/menu" exact element={<Menu />} />
+            <Route path="/contact" exact element={<Contact />} />
+            <Route path="/about" exact element={<About />} /> 
+            </Routes>
+        </div>
+    );
+}
+
+export default Body;
