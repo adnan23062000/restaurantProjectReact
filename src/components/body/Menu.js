2539ddf55ec56adbffa9c81466d890aa9961dfe6
Ahmed Adnan <bsse1131@iit.du.ac.bd>
2022-10-17 05:06:07 +0600

    redux store accessible from menu component

--- src/components/body/Menu.js
+++ src/components/body/Menu.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import DISHES from '../../data/dishes.js';
-import COMMENTS from '../../data/comments.js';
 import MenuItem from './MenuItem';
 import DishDetail from './DishDetail';
 import { CardColumns, Modal, ModalBody, ModalFooter, Button } from 'reactstrap';
+import { connect } from 'react-redux';
 
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments
+    }
+}
 
 class Menu extends Component {
     state = {
-        dishes: DISHES,
-        comments: COMMENTS,
         selectedDish: null,
         modalOpen: false
     }
@@ -29,8 +32,8 @@ class Menu extends Component {
 
     render() {
         document.title = "Menu";
-        
-        const menu = this.state.dishes.map(item => {
+
+        const menu = this.props.dishes.map(item => {
             return (
                 <MenuItem
                     dish={item}
@@ -42,7 +45,7 @@ class Menu extends Component {
 
         let dishDetail = null;
         if (this.state.selectedDish != null) {
-            const comments = this.state.comments.filter(comment => {
+            const comments = this.props.comments.filter(comment => {
                 return comment.dishId === this.state.selectedDish.id;
             });
             dishDetail = <DishDetail 
@@ -72,4 +75,4 @@ class Menu extends Component {
     }
 }
 
-export default Menu;
+export default connect(mapStateToProps)(Menu);
