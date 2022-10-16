3f149778981560c8da4c67361377e8f9a621cd78
Ahmed Adnan <bsse1131@iit.du.ac.bd>
2022-10-17 02:32:40 +0600

    footer added

--- src/components/body/Contact.js
+++ src/components/body/Contact.js
@@ -14,6 +14,22 @@ class Contact extends Component {
             contactType: 'Tel.',
             message: ""
         }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange = event => {
+        const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+        const name = event.target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = event => {
+        console.log(this.state);
+        event.preventDefault();
     }
     
     
@@ -21,20 +37,26 @@ class Contact extends Component {
     render() {
         return (
             <div className='container'>
-                <div className='row row-content'>
-                    <div className='col-12'>
+                <div className='row row-content' style={{paddingLeft: '20px', textAlign: 'left'}}>
+                    <div className='col-12'style={{paddingBottom: '20px'}}>
                         <h3>
                             Send us your feedback
                         </h3>
                     </div>
-                    <div className='col-12'>
-                        <Form>
+                    <div className='col-12 col-md-7'>
+                        <Form onSubmit={this.handleSubmit}>
                             <FormGroup row>
                                 <Label htmlFor='firstname' md={2}>
                                     FIrst Name
                                 </Label>
                                 <Col md={10}>
-                                    <Input type="text" name='firstname' placeholder='First Name' value={this.state.firstname}/>
+                                    <Input 
+                                    type="text" 
+                                    name='firstname' 
+                                    placeholder='First Name' 
+                                    value={this.state.firstname}
+                                    onChange={this.handleInputChange}
+                                    />
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -42,7 +64,12 @@ class Contact extends Component {
                                     Last Name
                                 </Label>
                                 <Col md={10}>
-                                    <Input type="text" name='lastname' placeholder='Last Name' value={this.state.lastname}/>
+                                    <Input type="text" 
+                                    name='lastname' 
+                                    placeholder='Last Name' 
+                                    value={this.state.lastname}
+                                    onChange={this.handleInputChange}
+                                    />
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -50,7 +77,13 @@ class Contact extends Component {
                                     Phone
                                 </Label>
                                 <Col md={10}>
-                                    <Input type="tel" name='phone' placeholder='Enter Phone no.' value={this.state.phone}/>
+                                    <Input 
+                                    type="tel" 
+                                    name='phone' 
+                                    placeholder='Enter Phone no.' 
+                                    value={this.state.phone}
+                                    onChange={this.handleInputChange}
+                                    />
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -58,19 +91,35 @@ class Contact extends Component {
                                     Email
                                 </Label>
                                 <Col md={10}>
-                                    <Input type="email" name='email' placeholder='Email' value={this.state.email}/>
+                                    <Input 
+                                    type="email" 
+                                    name='email' 
+                                    placeholder='Email' 
+                                    value={this.state.email}
+                                    onChange={this.handleInputChange}
+                                    />
                                 </Col>
                             </FormGroup>
-                            <FormGroup>
+                            <FormGroup row>
                                 <Col md={{size: 6, offset: 2}}>
                                     <FormGroup check>
                                         <Label check>
-                                            <Input type='checkbox' name="agree" checked={this.state.agree} /> <strong>May we contact you</strong>
+                                            <Input 
+                                            type='checkbox' 
+                                            name="agree" 
+                                            checked={this.state.agree} 
+                                            onChange={this.handleInputChange}
+                                            /> <strong>May we contact you?</strong>
                                         </Label>
                                     </FormGroup>
                                 </Col>
                                 <Col md={{size: 3, offset: 1}}>
-                                    <Input type='select' name='contactType' value={this.state.contactType}>
+                                    <Input 
+                                    type='select' 
+                                    name='contactType' 
+                                    value={this.state.contactType}
+                                    onChange={this.handleInputChange}
+                                    >
                                         <option>Tel.</option>
                                         <option>Email</option>
                                     </Input>
@@ -79,7 +128,13 @@ class Contact extends Component {
                             <FormGroup row>
                                 <Label htmlFor='message' md={2}>Your Feedback</Label>
                                 <Col md={10}>
-                                    <Input type="textarea" name='message' value={this.state.message} rows="12"></Input>
+                                    <Input 
+                                    type="textarea" 
+                                    name='message' 
+                                    value={this.state.message} 
+                                    rows="12"
+                                    onChange={this.handleInputChange}>
+                                    </Input>
                                 </Col>
                             </FormGroup>
                             <FormGroup>
